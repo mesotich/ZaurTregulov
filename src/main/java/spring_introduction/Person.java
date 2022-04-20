f2e9eb150e4f6cf1c9c56cf7c7d58ac8167a86ce
Dmitry Shishkin <4144442@bk.ru>
2022-04-20 22:16:31 +0300

    Dependency Injection с помощью сеттера

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -4,8 +4,16 @@ public class Person {
 
     private Pet pet;
 
+    public Person() {
+    }
+
     public Person(Pet pet) {
         System.out.println("Person been is created");
+        this.pet = pet;
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
         this.pet = pet;
     }
 
