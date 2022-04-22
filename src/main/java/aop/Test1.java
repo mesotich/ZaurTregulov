4c8bde4ffacb12193a80560e8853d0f301eb4d17
Dmitry Shishkin <4144442@bk.ru>
2022-04-22 07:40:50 +0300

    Комбинирование PointCut

--- src/main/java/aop/Test1.java
+++ src/main/java/aop/Test1.java
@@ -9,7 +9,8 @@ public class Test1 {
                 new AnnotationConfigApplicationContext(MyConfig.class);
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
         uniLibrary.getBook();
-        uniLibrary.getMagazine();
+        uniLibrary.returnMagazine();
+        uniLibrary.addBook();
 //        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
         //Book book = context.getBean("book", Book.class);
 //        uniLibrary.returnBook();
--- src/main/java/aop/aspects/LoggingAndSecurityAspect.java
+++ src/main/java/aop/aspects/LoggingAndSecurityAspect.java
@@ -9,21 +9,59 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAndSecurityAspect {
 
-    @Pointcut("execution(* get*())")
-    private void allGetters() {
+    @Pointcut("execution(* aop.UniLibrary.*(..))")
+    private void allMethodsFromUniLibrary() {
     }
 
-    @Before("allGetters()")//PointCut с Book и любым колличеством параметров
+    @Pointcut("execution(public void aop.UniLibrary.returnMagazine())")
+    private void returnMagazineFromUniLibrary() {
+    }
+
+    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
+    private void allMethodsFromUniLibraryWithoutReturnMagazine() {
+    }
+
+    @Before("allMethodsFromUniLibraryWithoutReturnMagazine()")
     public void beforeGetLoggingAdvice() {
-        System.out.println("beforeGetLoggingAdvice: попытка получить книгу/журнал");
+        System.out.println("allMethodsFromUniLibraryWithoutReturnMagazine: writing Log #10");
     }
-
-    @Before("allGetters()")
-    public void beforeGetSecurityAdvice() {
-        System.out.println("beforeGetSecurityAdvice: проверка прав на получение книги/журнала");
-    }
-//    @Before("execution(* returnBook())")
-//    public void beforeReturnBookAdvice() {
-//        System.out.println("beforeReturnBookAdvice: попытка вернуть книгу");
+//    @Pointcut("execution(* aop.UniLibrary.return*())")
+//    private void allReturnMethodsFromUniLibrary() {
 //    }
+//
+//    @Pointcut("allGetMethodsFromUniLibrary()||allReturnMethodsFromUniLibrary()")
+//    private void allGetAndReturnMethodsFromUniLibrary() {
+//    }
+//
+//    @Before("allGetMethodsFromUniLibrary()")
+//    public void beforeGetLoggingAdvice() {
+//        System.out.println("beforeGetLoggingAdvice: writing Log #1");
+//    }
+//
+//    @Before("allReturnMethodsFromUniLibrary()")
+//    public void beforeReturnLoggingAdvice() {
+//        System.out.println("beforeReturnLoggingAdvice: writing Log #2");
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromUniLibrary()")
+//    public void beforeGetOrReturnLoggingAdvice() {
+//        System.out.println("beforeGetOrReturnLoggingAdvice: writing Log #3");
+//    }
+//    @Pointcut("execution(* get*())")
+//    private void allGetters() {
+//    }
+//
+//    @Before("allGetters()")//PointCut с Book и любым колличеством параметров
+//    public void beforeGetLoggingAdvice() {
+//        System.out.println("beforeGetLoggingAdvice: попытка получить книгу/журнал");
+//    }
+//
+//    @Before("allGetters()")
+//    public void beforeGetSecurityAdvice() {
+//        System.out.println("beforeGetSecurityAdvice: проверка прав на получение книги/журнала");
+//    }
+////    @Before("execution(* returnBook())")
+////    public void beforeReturnBookAdvice() {
+////        System.out.println("beforeReturnBookAdvice: попытка вернуть книгу");
+////    }
 }
